66b44ef5ea8d7413ad77f9a35e16e0e99671fb4e
Augusto Sandim <augusto@jera.com.br>
2016-07-29 11:32:01 -0400

    Destrinchando funcionalidades ao carregar a view

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,10 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-    this._listaNegociacoes = new ListaNegociacoes();
+
+    this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+      this._negociacoesView.update(model);
+    });
 
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._negociacoesView.update(this._listaNegociacoes);
@@ -20,7 +23,6 @@ class NegociacaoController {
     event.preventDefault();
     
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._negociacoesView.update(this._listaNegociacoes);
 
     this._mensagem.texto = 'Negociação adicionada com sucesso';
     this._mensagemView.update(this._mensagem);
@@ -30,7 +32,6 @@ class NegociacaoController {
 
   apaga() {
     this._listaNegociacoes.esvazia();
-    this._negociacoesView.update(this._listaNegociacoes);
 
     this._mensagem.texto = 'Negociações apagadas com sucesso';
     this._mensagemView.update(this._mensagem);
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,21 @@
 class ListaNegociacoes {
 
-  constructor() {
+  constructor(contexto, armadilha) {
     this._negociacoes = [];
+    this._contexto = contexto;
+    this._armadilha = armadilha;
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
+    // this._armadilha(this);
+    Reflect.apply(this._armadilha, this._contexto, [this]);
   }
 
   esvazia() {
     this._negociacoes = [];
+    // this._armadilha(this);
+    Reflect.apply(this._armadilha, this._contexto, [this]);
   }
 
   get negociacoes() {
