fad9bdedf5ec3cdeed242bf2fb3dc82e16bc9279
Augusto Sandim <augusto@jera.com.br>
2016-07-30 00:03:20 -0400

    Ajuste na chamada da view

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,21 +1,20 @@
 class ListaNegociacoes {
 
-  constructor(contexto, armadilha) {
+  constructor( armadilha) {
     this._negociacoes = [];
-    this._contexto = contexto;
     this._armadilha = armadilha;
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
-    // this._armadilha(this);
-    Reflect.apply(this._armadilha, this._contexto, [this]);
+    this._armadilha(this);
+    // Reflect.apply(this._armadilha, this._contexto, [this]);
   }
 
   esvazia() {
     this._negociacoes = [];
-    // this._armadilha(this);
-    Reflect.apply(this._armadilha, this._contexto, [this]);
+    this._armadilha(this);
+    // Reflect.apply(this._armadilha, this._contexto, [this]);
   }
 
   get negociacoes() {
