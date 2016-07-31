184cffb29f02dd832f5ede5280e08fea9b510e06
Augusto Sandim <augusto@jera.com.br>
2016-07-31 01:01:42 -0400

    Separando regras de acesso ao servidor e requisições

---
+++ client/js/app/services/NegociacaoService.js
@@ -0,0 +1,28 @@
+class NegociacaoService {
+
+  obterNegociacoesDaSemana(callback) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'negociacoes/semana');
+
+    xhr.onreadystatechange = () => {
+
+      if(xhr.readyState == 4) {
+
+        if(xhr.status == 200) {
+          
+          callback(null, JSON.parse(xhr.responseText)
+            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+          
+        } else {
+          console.log(xhr.responseText);
+          callback('Não foi possivel obter as negociações');
+        }
+        
+      }
+    };
+
+    xhr.send();
+  }
+
+}
