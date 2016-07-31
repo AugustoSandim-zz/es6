fa6c930aac97ba016d9aac5f5a585c8d33fc593a
Augusto Sandim <augusto@jera.com.br>
2016-07-31 01:00:50 -0400

    Separando regras de acesso ao servidor e requisições

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -27,31 +27,19 @@ class NegociacaoController {
   }
 
   importaNegociacoes() {
-    let xhr = new XMLHttpRequest();
+    let service = new NegociacaoService();
 
-    xhr.open('GET', 'negociacoes/semana');
-
-    xhr.onreadystatechange = () => {
-
-      if(xhr.readyState == 4) {
-
-        if(xhr.status == 200) {
-          
-          JSON.parse(xhr.responseText)
-            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-          
-          this._mensagem.texto = 'Negociacoes importadas com sucesso.';
-
-        } else {
-          console.log(xhr.responseText);
-          this._mensagem.texto = 'Não foi possível obter as negociacoes da semana.';
-        }
-        
+    service.obterNegociacoesDaSemana((err, negociacoes) => {
+      
+      if(err) {
+        this._mensagem.texto = err;
+        return;
       }
-    }
 
-    xhr.send();
+      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+      this._mensagem.texto = 'Negociações importadas com sucesso';
+
+    });
   }
 
   apaga() {
